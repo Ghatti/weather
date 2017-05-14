90419a83bb1c4c163911f857c307e65503a6bf22
Ghatti <math.a.monteiro@gmail.com>
2017-05-14 09:14:28 -0300

    Add files via upload

--- js/weather.js
+++ js/weather.js
@@ -1,3 +1,6 @@
+var coords;
+var celsius = true;
+
 $(document).ready(function(){
 
    if (navigator.geolocation){
@@ -5,25 +8,53 @@ $(document).ready(function(){
         navigator.geolocation.getCurrentPosition(function(position){
 
 
-            var coords = [position.coords.latitude, position.coords.longitude];
+            coords = [position.coords.latitude, position.coords.longitude];
 
-            //alert("https://api.darksky.net/forecast/631081f27db94fb5a700895f671efc20/" + coords[0] + "," + coords[1] + "?exclude=currently,minutely,hourly,alerts,flags&units=si");
+            //&callback=?
 
-            $.getJSON("https://api.darksky.net/forecast/631081f27db94fb5a700895f671efc20/" + coords[0] + "," + coords[1], function(json){
+            
 
-
-                alert(worked);
-
-            });
+           $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + coords[0] + "&lon=" + coords[1] +"&units=metric&APPID=300559ed560870a7e312018973c6dc0c", function(json){
 
 
-
-
-
-
+                
+                $("#icon").attr("src", "assets/" + json.weather[0].icon + ".svg");
+                $("#sky").html("Sky condition: " + json.weather[0].main);
+                $("#local").html(json.name + "/" + json.sys.country);
+                $("#current").html("Temperature: " + json.main.temp + " ºC");
+                $("#speed").html("Wind speed: " + json.wind.speed + " m/s");
+                $("#humidity").html("Humidity: " + json.main.humidity + "%");
+            }); 
         });
-
-
    }
         
+
+   $("#change").click(function(){
+
+            
+        if(celsius){
+
+            celsius = false;
+            
+            $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + coords[0] + "&lon=" + coords[1] +"&units=imperial&APPID=300559ed560870a7e312018973c6dc0c", function(json){
+
+                $("#current").html("Temperature: " + json.main.temp + " ºF");
+                $("#change").html("Celsius");
+                $("#change").css("background-color", "blue");
+            });
+        }
+        else
+        {
+            celsius = true;
+            
+            $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + coords[0] + "&lon=" + coords[1] +"&units=metric&APPID=300559ed560870a7e312018973c6dc0c", function(json){
+
+                $("#current").html("Temperature: " + json.main.temp + " ºC");
+                $("#change").html("Fahrenheit");
+                $("#change").css("background-color", "red");
+            });
+        }
+
+   })
+
 });
